8a76c635b183ce252473cbbd3a427fe9d41b5a05
shihou22 <pc9801bx2@gmail.com>
2019-07-14 20:20:02 +0900

    practice C#

---
+++ src/easy/Merge Two Binary Trees/Solution.cs
@@ -0,0 +1,111 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Xml.Schema;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+
+namespace Merge_Two_Binary_Trees
+{
+  class Solution
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+    {
+      if (t1 == null && t2 == null)
+        return null;
+
+      if (t2 == null)
+        return t1;
+
+      if (t1 == null)
+        return t2;
+
+      t1.val += t2.val;
+
+      var queueT1 = new Queue<TreeNode>();
+      queueT1.Enqueue(t1);
+      var queueT2 = new Queue<TreeNode>();
+      queueT2.Enqueue(t2);
+      while (queueT1.Count > 0 || queueT2.Count > 0)
+      {
+        var inT1 = queueT1.Dequeue();
+        var inT2 = queueT2.Dequeue();
+
+        if (inT1 == null || inT2 == null)
+          continue;
+
+        if (inT1.left != null && inT2.left != null)
+          inT1.left.val += inT2.left.val;
+
+        queueT1.Enqueue(inT1.left);
+        queueT2.Enqueue(inT2.left);
+
+        if (inT1.left == null)
+          inT1.left = inT2.left;
+
+        if (inT1.right != null && inT2.right != null)
+          inT1.right.val += inT2.right.val;
+
+        queueT1.Enqueue(inT1.right);
+        queueT2.Enqueue(inT2.right);
+
+        if (inT1.right == null)
+          inT1.right = inT2.right;
+      }
+      return t1;
+    }
+
+    public TreeNode MergeTreesRevursive(TreeNode t1, TreeNode t2)
+    {
+      if (t1 == null && t2 == null)
+        return null;
+
+      if (t2 == null)
+        return t1;
+
+      if (t1 == null)
+        return t2;
+
+      t1.val += t2.val;
+      recursiveCall(t1, t2);
+      return t1;
+    }
+
+    private void recursiveCall(TreeNode left, TreeNode right)
+    {
+      if (left == null || right == null)
+        return;
+
+      if (left.left != null && right.left != null)
+        left.left.val += right.left.val;
+
+
+      if (left.right != null && right.right != null)
+        left.right.val += right.right.val;
+
+
+      recursiveCall(left.left, right.left);
+      recursiveCall(left.right, right.right);
+
+      if (left.left == null)
+        left.left = right.left;
+
+      if (left.right == null)
+        left.right = right.right;
+
+    }
+  }
+
+
+  public class TreeNode
+  {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+  }
+}
---
+++ src/medium/Binary Tree Level Order Traversal/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Binary_Tree_Level_Order_Traversal
+{
+  class Solution
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+      var res = new List<IList<int>>();
+      Queue<TreeNode> queue = new Queue<TreeNode>();
+      queue.Enqueue(root);
+      int depth = 1;
+      while (queue.Count > 0)
+      {
+        var tmpRes = new List<int>();
+        var max = queue.Count;
+        for (int i = 0; i < max; i++)
+        {
+          var tmp = queue.Dequeue();
+          if (tmp == null)
+            continue;
+
+          tmpRes.Add(tmp.val);
+          queue.Enqueue(tmp.left);
+          queue.Enqueue(tmp.right);
+        }
+        depth++;
+        if (tmpRes.Count > 0)
+          res.Add(tmpRes);
+      }
+      return res;
+    }
+
+    public IList<IList<int>> LevelOrderRecursive(TreeNode root)
+    {
+      return recursive(root, new List<IList<int>>(), 1);
+    }
+
+    public IList<IList<int>> recursive(TreeNode root, IList<IList<int>> res, int depth)
+    {
+      if (root == null)
+        return res;
+
+      if (depth > res.Count)
+        res.Add(new List<int>());
+
+      var tmp = res[depth - 1];
+
+      tmp.Add(root.val);
+
+      recursive(root.left, res, depth + 1);
+      recursive(root.right, res, depth + 1);
+      return res;
+    }
+  }
+  public class TreeNode
+  {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+  }
+}
