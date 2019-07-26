a62977f1223ff82c3c52af32e740b2f0f150d50a
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-07-26 16:59:50 +0900

    practice

---
+++ src/medium/Longest Increasing Subsequence/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Longest_Increasing_Subsequence
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int LengthOfLIS(int[] nums)
+        {
+
+        }
+    }
+}
