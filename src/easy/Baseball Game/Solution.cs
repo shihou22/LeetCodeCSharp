c40dba4f2c2f7c72708c8a9b4fd0cca29a277868
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-07-29 19:04:10 +0900

    practice

---
+++ src/easy/Baseball Game/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Baseball_Game
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.CalPoints(new string[] { "5", "2", "C", "D", "+" }));//30
+            Console.WriteLine(solution.CalPoints(new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" }));//27
+            Console.WriteLine("Hello World!");
+        }
+        public int CalPoints(string[] ops)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (var item in ops)
+            {
+                var wk1 = 0;
+                var wk2 = 0;
+                switch (item)
+                {
+                    case "+":
+                        wk1 = stack.Pop();
+                        wk2 = stack.Pop();
+                        stack.Push(wk2);
+                        stack.Push(wk1);
+                        stack.Push(wk1 + wk2);
+                        break;
+                    case "C":
+                        if (stack.Count != 0)
+                            stack.Pop();
+                        break;
+                    case "D":
+                        if (stack.Count != 0)
+                        {
+                            wk1 = stack.Pop();
+                            stack.Push(wk1);
+                            stack.Push(wk1 * 2);
+                        }
+                        break;
+                    default:
+                        stack.Push(int.Parse(item));
+                        break;
+                }
+
+            }
+            int res = 0;
+            foreach (var item in stack)
+            {
+                res += item;
+            }
+            return res;
+        }
+    }
+}
---
+++ src/easy/Print in Order/Foo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Print_in_Order
+{
+    public class Foo
+    {
+        private System.Threading.Tasks.TaskCompletionSource<object> firstCompletionSource = new System.Threading.Tasks.TaskCompletionSource<object>();
+        private System.Threading.Tasks.TaskCompletionSource<object> secondCompletionSource = new System.Threading.Tasks.TaskCompletionSource<object>();
+
+        public Foo()
+        {
+
+        }
+
+        public void First(Action printFirst)
+        {
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+            firstCompletionSource.SetResult(new object());
+        }
+
+        public void Second(Action printSecond)
+        {
+            var ignored = firstCompletionSource.Task.Result;
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+            secondCompletionSource.SetResult(new object());
+        }
+
+        public void Third(Action printThird)
+        {
+            var ignored = secondCompletionSource.Task.Result;
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
+    }
+}
