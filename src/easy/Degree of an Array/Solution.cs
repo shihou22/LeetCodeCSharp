09805871990ddf8e3d22a18baa2df26d99140baf
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-07-30 20:04:56 +0900

    practice

---
+++ src/easy/Degree of an Array/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Degree_of_an_Array
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.FindShortestSubArray(new int[] { 1, 2, 2, 3, 1 }));//2
+            Console.WriteLine("Hello World!");
+        }
+        public int FindShortestSubArray(int[] nums)
+        {
+
+        }
+    }
+}
