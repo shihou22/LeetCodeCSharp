c8c4f3e2162de0f9ced103a6bda9753455c21626
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-08-01 13:43:23 +0900

    practice

---
+++ src/easy/Range Addition II/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Range_Addition_II
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int MaxCount(int m, int n, int[][] ops)
+        {
+            int mMax = m;
+            int nMax = n;
+
+            foreach (var item in ops)
+            {
+                mMax = Math.Min(mMax, item[0]);
+                nMax = Math.Min(nMax, item[1]);
+            }
+            return mMax * nMax;
+        }
+        public int MaxCountOOM(int m, int n, int[][] ops)
+        {
+            int[,] map = new int[m, n];
+            for (int i = 0; i < ops.Length; i++)
+            {
+                int[] wk = ops[i];
+                for (int j = 0; j < wk[0]; j++)
+                {
+                    for (int k = 0; k < wk[1]; k++)
+                    {
+                        map[j, k]++;
+                    }
+                }
+            }
+            int max = 0;
+            foreach (var item in map)
+            {
+                max = Math.Max(max, item);
+            }
+            int res = 0;
+            foreach (var item in map)
+            {
+                if (item == max)
+                    res++;
+            }
+            return res;
+        }
+    }
+}
---
+++ src/medium/Largest Values From Labels/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Largest_Values_From_Labels
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var wk1 = solution.LargestValsFromLabels(new int[] { 5, 4, 3, 2, 1 }, new int[] { 1, 1, 2, 2, 3 }, 3, 1);
+            Console.WriteLine(wk1);//9
+            var wk2 = solution.LargestValsFromLabels(new int[] { 5, 4, 3, 2, 1 }, new int[] { 1, 3, 3, 3, 2 }, 3, 2);
+            Console.WriteLine(wk2);//12
+            var wk3 = solution.LargestValsFromLabels(new int[] { 9, 8, 8, 7, 6 }, new int[] { 0, 0, 0, 1, 1 }, 3, 1);
+            Console.WriteLine(wk3);//16
+            var wk4 = solution.LargestValsFromLabels(new int[] { 9, 8, 8, 7, 6 }, new int[] { 0, 0, 0, 1, 1 }, 3, 2);
+            Console.WriteLine(wk4);//24
+            Console.WriteLine("Hello World!");
+        }
+        public int LargestValsFromLabels(int[] values, int[] labels, int num_wanted, int use_limit)
+        {
+
+        }
+    }
+}
