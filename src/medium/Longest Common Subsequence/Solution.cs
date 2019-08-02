8b3eb0c566cefee25a323337b505fe748f6aa11b
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-08-02 18:39:44 +0900

    practice

---
+++ src/medium/Longest Common Subsequence/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Longest_Common_Subsequence
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            int[,] dp = new int[text1.Length + 1, text2.Length + 1];
+            for (int i = 1; i <= text1.Length; i++)
+            {
+                for (int j = 1; j <= text2.Length; j++)
+                {
+                    if (text1[i - 1].Equals(text2[j - 1]))
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    else
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+            return dp[text1.Length, text2.Length];
+        }
+    }
+}
