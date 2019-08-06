045789e95fa731eb67ec995ff329fbc0e8b595dd
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-08-06 14:23:41 +0900

    practice

---
+++ src/easy/Remove All Adjacent Duplicates In String/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Remove_All_Adjacent_Duplicates_In_String
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.RemoveDuplicates("abbaca"));//ca
+            Console.WriteLine("Hello World!");
+        }
+        public string RemoveDuplicates(string S)
+        {
+            Stack<char> memo = new Stack<char>();
+            foreach (var item in S)
+            {
+                if (memo.Count == 0)
+                {
+                    memo.Push(item);
+                    continue;
+                }
+                var temp = memo.Peek();
+                if (temp != item)
+                    memo.Push(item);
+                else
+                    memo.Pop();
+            }
+            StringBuilder res = new StringBuilder();
+            foreach (var item in memo.ToArray().Reverse())
+            {
+                res.Append(item);
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ src/easy/Two City Scheduling/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Two_City_Scheduling
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[][] input = new int[][] {
+            new []{259, 770},
+            new []{448, 54},
+            new[] { 926, 667},
+            new[] { 184, 139},
+            new []{840, 118},
+            new []{577, 469}
+        };
+            Console.WriteLine(solution.TwoCitySchedCost(input));
+            Console.WriteLine("Hello World!");
+        }
+        public int TwoCitySchedCost(int[][] costs)
+        {
+            int res = 0;
+            foreach (var item in costs)
+            {
+                res += item[0] < item[1] ? item[0] : item[1];
+            }
+            return res;
+        }
+    }
+}
