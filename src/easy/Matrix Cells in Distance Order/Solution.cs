4fdee6018dcef1d426f6a427786bd96097dee1a3
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-08-09 21:08:08 +0900

    practice

--- src/easy/Matrix Cells in Distance Order/Solution.cs
+++ src/easy/Matrix Cells in Distance Order/Solution.cs
@@ -24,42 +24,31 @@ namespace Matrix_Cells_in_Distance_Order
         }
         public int[][] AllCellsDistOrder(int R, int C, int r0, int c0)
         {
-            bool[,] visited = new bool[R, C];
-            int[][] res = new int[R * C][];
-            int wkR = r0;
-            int wkC = c0;
+            List<int[]> res = new List<int[]>();
             Queue<Coordinate> que = new Queue<Coordinate>();
-            que.Enqueue(new Coordinate(0, 0));
-            int cnt = 0;
-            while (que.Count != 0)
-            {
-                var elm = que.Dequeue();
-                // if (elm.r >= R || elm.c >= C)
-                //     continue;
-                res[cnt] = new int[] { Math.Abs(elm.r - wkR), Math.Abs(elm.c - wkC) };
-                cnt++;
-                visited[elm.r, elm.c] = true;
+            bool[,] visited = new bool[R, C];
 
-                if (elm.c + 1 < C && !visited[elm.r, elm.c + 1])
-                {
-                    visited[elm.r, elm.c + 1] = true;
-                    que.Enqueue(new Coordinate(elm.r, elm.c + 1));
-                }
-                if (elm.r + 1 < R && !visited[elm.r + 1, elm.c])
-                {
-                    visited[elm.r + 1, elm.c] = true;
-                    que.Enqueue(new Coordinate(elm.r + 1, elm.c));
-                }
+            que.Enqueue(new Coordinate(r0, c0));
+            visited[r0, c0] = true;
+            while (que.Count > 0)
+            {
+                Coordinate current = que.Dequeue();
+                res.Add(new int[] { current.r, current.c });
+                AddQueue(R, C, current.r - 1, current.c, visited, que);
+                AddQueue(R, C, current.r + 1, current.c, visited, que);
+                AddQueue(R, C, current.r, current.c - 1, visited, que);
+                AddQueue(R, C, current.r, current.c + 1, visited, que);
             }
-            // for (int i = 0; i < C; i++)
-            // {
-            //     for (int j = 0; j < R; j++)
-            //     {
-            //         res[cnt] = new int[] { Math.Abs(j - wkR), Math.Abs(i - wkC) };
-            //         cnt++;
-            //     }
-            // }
-            return res;
+            return res.ToArray();
+        }
+
+        private void AddQueue(int R, int C, int wkR, int wkC, bool[,] visited, Queue<Coordinate> que)
+        {
+            if (wkR >= 0 && wkR < R && wkC >= 0 && wkC < C && !visited[wkR, wkC])
+            {
+                que.Enqueue(new Coordinate(wkR, wkC));
+                visited[wkR, wkC] = true;
+            }
         }
     }
 }
