62673fdd9489606f0d5c0b4f666ec3d82df0e02d
shihou22 <pc9801bx2@gmail.com>
2019-08-15 16:18:44 +0900

    practice

---
+++ src/easy/Range Sum of BST/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Range_Sum_of_BST
+{
+  class Solution
+  {
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+    public int RangeSumBST(TreeNode root, int L, int R)
+    {
+      int sum = 0;
+      if (root == null)
+        return sum;
+      int wk = root.val;
+      sum += (L <= wk && wk <= R) ? wk : 0;
+      sum += RangeSumBST(root.left, L, R);
+      sum += RangeSumBST(root.right, L, R);
+      return sum;
+    }
+    public int RangeSumBSTBFS(TreeNode root, int L, int R)
+    {
+      Stack<TreeNode> stack = new Stack<TreeNode>();
+      stack.Push(root);
+      int sum = 0;
+      while (stack.Count > 0)
+      {
+        TreeNode wkNode = stack.Pop();
+        if (wkNode == null)
+          continue;
+        if (L <= wkNode.val && wkNode.val <= R)
+        {
+          sum += wkNode.val;
+        }
+        stack.Push(wkNode.left);
+        stack.Push(wkNode.right);
+      }
+      return sum;
+    }
+  }
+}
---
+++ src/medium/Permutation Sequence/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Collections.Immutable;
+using System;
+using System.Collections.Generic;
+
+namespace Permutation_Sequence
+{
+  class Solution
+  {
+    static void Main(string[] args)
+    {
+      Solution solution = new Solution();
+      Console.WriteLine(solution.GetPermutation(3, 3));//213
+      Console.WriteLine("Hello World!");
+    }
+    public string GetPermutation(int n, int k)
+    {
+      IList<int> res = new List<int>();
+      int[] nums = new int[n];
+      for (int i = 0; i < n; i++)
+      {
+        nums[i] = i + 1;
+      }
+      this.k = k;
+      return RecPerm(nums, n, new bool[n], 0, 0).ToString();
+    }
+    int k;
+    private int RecPerm(int[] nums, int n, bool[] visited, int current, int num)
+    {
+
+      if (current == n)
+      {
+        k--;
+        if (k == 0)
+        {
+          return num;
+        }
+        return -1;
+      }
+      for (int i = 0; i < nums.Length; i++)
+      {
+        if (visited[i])
+          continue;
+        visited[i] = true;
+        int wkNum = num * 10 + nums[i];
+        int res = RecPerm(nums, n, visited, current + 1, wkNum);
+        if (res != -1)
+        {
+          return res;
+        }
+        visited[i] = false;
+      }
+      return -1;
+    }
+  }
+
+}
