3dfa82d8d7eb79fa99d75a6e10e27f43b164795f
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-08-23 17:51:31 +0900

    practice

---
+++ src/easy/Rotated Digits/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rotated_Digits
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            // Console.WriteLine(solution.RotatedDigits(10));//4
+            Console.WriteLine(solution.GetRoatedNum(111));
+            // Console.WriteLine(solution.RotatedDigits(10));//4
+            Console.WriteLine("Hello World!");
+        }
+        public int RotatedDigits(int N)
+        {
+            int res = 0;
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 1; i <= N; i++)
+            {
+                if (GetRoatedNum(i))
+                    res++;
+            }
+            return res;
+        }
+        private bool GetRoatedNum(int i)
+        {
+            bool isExistRotateNum = false;
+            while (i > 0)
+            {
+                int wk = i % 10;
+                switch(wk){
+                    case 2:
+                    case 5:
+                    case 6:
+                    case 9:
+                    isExistRotateNum = true;
+                    break;
+                    case 1:
+                    case 8:
+                    break;
+                    case 3:
+                    case 4:
+                    case 7:
+                    return false;
+                }
+                i /= 10;
+            }
+            return isExistRotateNum;
+        }
+    }
+}
---
+++ src/medium/Longest Uncommon Subsequence II/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Longest_Uncommon_Subsequence_II
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.FindLUSlength(new string[] { "aba", "cdc", "eae" }));//3
+            Console.WriteLine("Hello World!");
+        }
+        public int FindLUSlength(string[] strs)
+        {
+            if (strs.Length == 1)
+                return strs.Length;
+
+            string baseStr = strs[0];
+            foreach (var item in strs)
+            {
+                string wk1 = "";
+                string wk2 = "";
+                if (baseStr.Length > item.Length)
+                {
+                    wk1 = item;
+                    wk2 = baseStr;
+                }
+                else
+                {
+                    wk1 = baseStr;
+                    wk2 = item;
+                }
+                baseStr = GetCheck(wk1, wk2);
+                if (baseStr.Length <= 0)
+                    return 0;
+            }
+            return baseStr.Length;
+        }
+        private string GetCheck(string wk1, string wk2)
+        {
+            if (wk1.Length <= 0)
+                return wk1;
+
+            int index = wk2.IndexOf(wk1);
+            if (index < 0)
+            {
+                if (wk1.Length >= 2)
+                {
+                    string val1 = GetCheck(wk1.Substring(1, wk1.Length - 1), wk2);
+                    string val2 = GetCheck(wk1.Substring(0, wk1.Length - 2), wk2);
+                    return val1.Length > val2.Length ? val1 : val2;
+                }
+                else
+                {
+                    return "";
+                }
+
+            }
+            else
+            {
+                return wk1;
+            }
+
+        }
+    }
+}
