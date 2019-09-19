32ff038201314aa1b84d8ef3d33cee517f7ad6ce
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-09-19 16:20:06 +0900

    practice

---
+++ src/easy/Subtree of Another Tree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subtree_of_Another_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null)
+                return true;
+            if (s == null || t == null)
+                return false;
+            var res1 = DFSFirst(s);
+            var res2 = DFSFirst(t);
+            // Console.WriteLine(res1);
+            // Console.WriteLine(res2);
+            return res2.IndexOf(res1) >= 0;
+        }
+
+        private string DFSFirst(TreeNode root)
+        {
+            if (root == null)
+                return "|null|";
+            var res = "|" + root.val.ToString() + "|";
+            var val1 = DFSFirst(root.left);
+            var val2 = DFSFirst(root.right);
+            return res + val1 + val2;
+        }
+        private bool DFSSecond(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null)
+                return true;
+            if (s == null && t != null || s != null && t == null)
+                return false;
+
+            if (s.val != t.val)
+                return false;
+
+            var val1 = DFSSecond(s.left, t.left);
+            var val2 = DFSSecond(s.left, t.left);
+            return val1 && val2;
+        }
+    }
+}
