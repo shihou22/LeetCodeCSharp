14879d800e238347d5f5a7d1abba69b552f45fe5
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-09-24 14:07:24 +0900

    practice

---
+++ src/easy/Day of the Week/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_of_the_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public string DayOfTheWeek(int day, int month, int year)
+        {
+            var theDay = new DateTime(year, month, day);
+            // string dayLong = string.Format("{0:dddd}", theDay);
+            // return dayLong;
+            return theDay.DayOfWeek.ToString();
+        }
+    }
+}
