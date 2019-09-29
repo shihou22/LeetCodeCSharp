8e512576a9fd545d580ed8395caf242bf123f669
shihou22 <pc9801bx2@gmail.com>
2019-09-29 20:46:21 +0900

    practice

---
+++ src/easy/Longest Univalue Path/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Longest_Univalue_Path
+{
+  public class TreeNode
+  {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+    public int LongestUnivaluePath(TreeNode root)
+    {
+      DFS(root);
+      return res;
+    }
+    int res = 0;
+    private int DFS(TreeNode root)
+    {
+      if (root == null)
+        return 0;
+      int left = DFS(root.left);
+      int right = DFS(root.right);
+
+      int wkLeft = 0;
+      int wkRight = 0;
+
+      //同じ値の時のみ対象とする
+      if (root.left != null && root.val == root.left.val)
+        wkLeft = left + 1;
+      //同じ値の時のみ対象とする
+      if (root.right != null && root.val == root.right.val)
+        wkRight = right + 1;
+
+      res = Math.Max(res, wkLeft + wkRight);
+      return Math.Max(wkLeft, wkRight);
+    }
+  }
+}
---
+++ src/easy/Second Minimum Node In a Binary Tree/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Second_Minimum_Node_In_a_Binary_Tree
+{
+  public class TreeNode
+  {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+    public int FindSecondMinimumValue(TreeNode root)
+    {
+      SortedDictionary<int, int> memo = new SortedDictionary<int, int>();
+      Queue<TreeNode> que = new Queue<TreeNode>();
+      que.Enqueue(root);
+      while (que.Count > 0)
+      {
+        var wk = que.Dequeue();
+        if (wk == null)
+          continue;
+        memo.TryAdd(wk.val, 0);
+        memo[wk.val]++;
+        que.Enqueue(wk.left);
+        que.Enqueue(wk.right);
+      }
+      int cnt = 1;
+      foreach (var item in memo)
+      {
+        if (cnt == 2)
+          return item.Key;
+        cnt++;
+      }
+      return -1;
+    }
+  }
+}
