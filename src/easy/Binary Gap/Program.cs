dcebeaadc02ff6d6c3d2f96b3cdd00e29cd86b89
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-10-03 15:49:18 +0900

    practice

--- src/easy/Binary Gap/Program.cs
+++ src/easy/Binary Gap/Program.cs
@@ -8,5 +8,27 @@ namespace Binary_Gap
         {
             Console.WriteLine("Hello World!");
         }
+        public int BinaryGap(int N)
+        {
+            List<int> wk = new List<int>();
+            while (N > 0)
+            {
+                wk.Add(N & 1);
+                N >>= 1;
+            }
+            int ret = 0;
+            int left = 0;
+            int right = 1;
+            while (left < right && left < wk.Count - 1)
+            {
+                while (right < wk.Count && wk[right] == 1)
+                    right++;
+
+                ret = Math.Max(ret, right - left);
+                left = right;
+                right++;
+            }
+            return ret;
+        }
     }
 }
