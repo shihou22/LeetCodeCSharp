8c9d962aa9bbf572d21e42e165e61f181cab1cef
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-10-03 16:07:31 +0900

    practice

--- src/easy/Binary Gap/Program.cs
+++ src/easy/Binary Gap/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Binary_Gap
 {
@@ -6,25 +7,56 @@ namespace Binary_Gap
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            Console.WriteLine(program.BinaryGap(22));//2
+            Console.WriteLine(program.BinaryGap(5));//2
+            Console.WriteLine(program.BinaryGap(6));//1
+            Console.WriteLine(program.BinaryGap(8));//0
             Console.WriteLine("Hello World!");
         }
         public int BinaryGap(int N)
         {
+            int ret = 0;
+            //cnt==0は初回判定に使う
+            int cnt = 0;
+            while (N > 0)
+            {
+                if ((N & 1) == 1)
+                {
+                    //初回は不要
+                    if (cnt > 0)
+                    {
+                        ret = Math.Max(cnt, ret);
+                    }
+                    cnt = 1;
+                }
+                //初回は不要
+                else if (cnt > 0)
+                {
+                    cnt++;
+                }
+                N >>= 1;
+            }
+            return ret;
+        }
+        public int BinaryGapSlidingWindow(int N)
+        {
             List<int> wk = new List<int>();
             while (N > 0)
             {
-                wk.Add(N & 1);
+                wk.Insert(0, N & 1);
                 N >>= 1;
             }
             int ret = 0;
             int left = 0;
             int right = 1;
-            while (left < right && left < wk.Count - 1)
+            while (left < right && right < wk.Count)
             {
-                while (right < wk.Count && wk[right] == 1)
+                while (right < wk.Count && wk[right] == 0)
                     right++;
 
-                ret = Math.Max(ret, right - left);
+                if (right < wk.Count && wk[right] != 0)
+                    ret = Math.Max(ret, right - left);
                 left = right;
                 right++;
             }
