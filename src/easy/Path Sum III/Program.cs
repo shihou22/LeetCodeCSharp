6e46e5e9ec61b177f1f86dfb8248b8c6a92b260d
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-10-15 14:55:59 +0900

    practice

---
+++ src/easy/Path Sum III/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Path_Sum_III
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int PathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+                return 0;
+            return Helper(root, sum) + PathSum(root.left, sum) + PathSum(root.right, sum);
+        }
+
+        private int Helper(TreeNode root, int sum)
+        {
+            if (root == null)
+                return 0;
+
+            int count = 0;
+            if (root.val == sum)
+                count++;
+            count += Helper(root.left, sum - root.val);
+            count += Helper(root.right, sum - root.val);
+            return count;
+        }
+    }
+}
