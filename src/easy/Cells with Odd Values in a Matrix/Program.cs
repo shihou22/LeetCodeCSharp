0fe7219fa347c7e4c96babf29fd06ba2ee5de4f8
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-12-18 18:09:16 +0900

    practice

---
+++ src/easy/Cells with Odd Values in a Matrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cells_with_Odd_Values_in_a_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
