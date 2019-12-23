8922bd78f8b54439d14ddc62e452057d478c4405
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-12-23 14:59:45 +0900

    practice

--- src/easy/Check If It Is a Straight Line/Program.cs
+++ src/easy/Check If It Is a Straight Line/Program.cs
@@ -10,23 +10,52 @@ namespace Check_If_It_Is_a_Straight_Line
         }
         public bool CheckStraightLine(int[][] coordinates)
         {
-            Array.Sort(coordinates, (a, b) => a[0] < b[0] ? -1 : a[0] == b[0] ? 0 : 1);
             if (coordinates == null || coordinates.Length <= 2)
                 return true;
-            int pX = Math.Abs(Math.Abs(coordinates[1][0]) - Math.Abs(coordinates[0][0]));
-            int pY = Math.Abs(Math.Abs(coordinates[1][1]) - Math.Abs(coordinates[0][1]));
-            pX = pX == 0 ? 1 : pX;
-            pY = pY == 0 ? 1 : pY;
-            // Console.WriteLine("pX/pY : " + pX + "/" + pY);
-            for (int i = 2; i < coordinates.Length; i++)
+
+            Array.Sort(coordinates, (a, b) => a[0] < b[0] ? -1 : a[0] == b[0] ? 0 : 1);
+            int pX = coordinates[0][0] - coordinates[1][0];
+            int pY = coordinates[0][1] - coordinates[1][1];
+            if (pX == 0 && pY != 0)
             {
-                if ((coordinates[i][0] - coordinates[i - 1][0]) % pX != 0
-                || (coordinates[i][1] - coordinates[i - 1][1]) % pY != 0)
+                pX = coordinates[0][0];
+                foreach (var item in coordinates)
                 {
-                    // Console.WriteLine((coordinates[i][0] - coordinates[i - 1][0]) % pX);
-                    // Console.WriteLine((coordinates[i][1] - coordinates[i - 1][1]) % pY);
-                    // Console.WriteLine("coordinates[i-1][0]/coordinates[i-1][1] : " + coordinates[i - 1][0] + "/" + coordinates[i - 1][1]);
-                    // Console.WriteLine("coordinates[i][0]/coordinates[i][1] : " + coordinates[i][0] + "/" + coordinates[i][1]);
+                    if (item[0] != pX)
+                        return false;
+                }
+                return true;
+            }
+            else if (pX != 0 && pY == 0)
+            {
+                pY = coordinates[0][1];
+                foreach (var item in coordinates)
+                {
+                    if (item[1] != pY)
+                        return false;
+                }
+                return true;
+            }
+            else if (pX == 0 && pY == 0)
+            {
+                pX = coordinates[0][0];
+                pY = coordinates[0][1];
+                foreach (var item in coordinates)
+                {
+                    if (item[0] != pX || item[1] != pY)
+                        return false;
+                }
+                return true;
+            }
+            Console.WriteLine(" pX/pY : " + pX + "/" + pY);
+            int pB = coordinates[0][1] - coordinates[0][0] * pY / pX;
+            Console.WriteLine("pB: " + pB);
+
+            foreach (var item in coordinates)
+            {
+                Console.WriteLine("item[0] *item[1] : " + item[0] + "/" + (item[1]));
+                if (item[1] - item[0] * pY / pX - pB != 0.0d)
+                {
                     return false;
                 }
             }
---
+++ src/sample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace sample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1: " + fact(4));
+            Console.WriteLine("2: " + fact2(4, 1));
+        }
+
+        static int fact(int i)
+        {
+            if (i == 0)
+                return 1;
+            return fact(i - 1) * i;
+        }
+        static int fact2(int i, int res)
+        {
+            if (i == 0)
+                return res;
+            return fact2(i - 1, res * i);
+        }
+        static void HogeMain(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            long A = long.Parse(s[0]);
+            long B = long.Parse(s[1]);
+            long X = long.Parse(s[2]);
+
+            if (A + B > X)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            int left = 1;
+            int right = 1000000001;
+            while (right - left > 1)
+            {
+                int mid = left + (right - left) / 2;
+                long cost = A * mid + B * numDigits(mid);
+                if (cost > X)
+                    right = mid;
+                else
+                    left = mid;
+
+            }
+            Console.WriteLine(left);
+        }
+
+        private static int numDigits(int a)
+        {
+            int d = 0;
+            while (a > 0)
+            {
+                d++;
+                a /= 10;
+            }
+            return d;
+        }
+
+    }
+}
