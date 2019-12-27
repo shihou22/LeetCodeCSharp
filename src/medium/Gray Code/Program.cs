c1a4249c92f9b627a6ca15590d3f6e48ace883b7
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-12-27 18:12:09 +0900

    practice

--- src/medium/Gray Code/Program.cs
+++ src/medium/Gray Code/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gray_Code
 {
@@ -6,7 +7,22 @@ namespace Gray_Code
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            Console.WriteLine(program.GrayCode(2));
+            Console.WriteLine(0 ^ (0 >> 1));
+            Console.WriteLine(1 ^ (1 >> 1));
+            Console.WriteLine(2 ^ (2 >> 1));
+            Console.WriteLine(3 ^ (3 >> 1));
             Console.WriteLine("Hello World!");
+        }
+        public IList<int> GrayCode(int n)
+        {
+            IList<int> res = new List<int>();
+            for (int i = 0; i < 1 << n; i++)
+            {
+                res.Add(i ^ (i >> 1));
+            }
+            return res;
         }
     }
 }
