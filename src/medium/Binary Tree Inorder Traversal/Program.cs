4edb5608f92bd8a91e010c466cd2ce15fbf3ef83
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2019-12-28 21:18:29 +0900

    practice

---
+++ src/medium/Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Binary_Tree_Inorder_Traversal
+{
+    /**
+    * Definition for a binary tree node.
+    */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            TreeNode node = new TreeNode(1);
+            node.right = new TreeNode(2);
+            node.right.left = new TreeNode(3);
+
+            var res = program.InorderTraversal(node);
+            Console.WriteLine("Hello World!");
+        }
+        /**
+        Recursive
+        */
+
+        public IList<int> InorderTraversalRecursive(TreeNode root)
+        {
+            IList<int> res = new List<int>();
+            helper(root, res);
+            return res;
+        }
+        public void helper(TreeNode root, IList<int> res)
+        {
+            if (root != null)
+            {
+                if (root.left != null)
+                {
+                    helper(root.left, res);
+                }
+                res.Add(root.val);
+                if (root.right != null)
+                {
+                    helper(root.right, res);
+                }
+            }
+        }
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> res = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode node = root;
+            while (node != null || stack.Count > 0)
+            {
+                while (node != null)
+                {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                node = stack.Pop();
+                res.Add(node.val);
+                node = node.right;
+            }
+            return res;
+        }
+
+    }
+}
---
+++ src/medium/Single Number II/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Single_Number_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wk = new List<int>() { 1, 2, 3, 4, 1, 2, 3 };
+            Program program = new Program();
+            int[] tmp = new int[] { 2, 2, 3, 2 };
+            Console.WriteLine(program.SingleNumber(tmp));
+            Console.WriteLine("Hello World!");
+        }
+        public int SingleNumber(int[] nums)
+        {
+            int res = 0;
+            //32bit確認
+            for (int i = 0; i < 32; i++)
+            {
+                int sum = 0;
+                //全ての値のbitを順に調べていく
+                foreach (var item in nums)
+                {
+                    if (((item >> i) & 1) == 1)
+                        sum++;
+                }
+                //3の倍数でまとめることができる
+                if (sum % 3 != 0)
+                    res |= (1 << i);
+            }
+            return res;
+        }
+    }
+}
