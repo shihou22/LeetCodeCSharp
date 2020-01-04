cac5b3a09463db06d9d049bb3fb5dfa02d9c8669
SUZUKI Ryo(鈴木 亮) <ryo.suzuki@avantcorp.com>
2020-01-04 19:20:53 +0900

    practice

---
+++ src/medium/Integer Break/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Integer_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int IntegerBreak(int n)
+        {
+
+        }
+    }
+}
