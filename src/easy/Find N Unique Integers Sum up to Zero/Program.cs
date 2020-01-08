18301b4bbca28d804c46f2d32039b4e7f10f23d0
shihou22 <pc9801bx2@gmail.com>
2020-01-08 10:21:20 +0900

    practice

---
+++ src/easy/Find N Unique Integers Sum up to Zero/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Find_N_Unique_Integers_Sum_up_to_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int[] SumZero(int n)
+        {
+            int[] res = new int[n];
+            if (n % 2 == 0)
+                res[n / 2] = 0;
+            for (int i = 0; i < n / 2; i++)
+            {
+                res[i] = n - i;
+                res[n - 1 - i] = -(n - i);
+            }
+            return res;
+        }
+
+        public int[] SumZero2(int n)
+        {
+            int[] res = new int[n];
+            for (int i = 0; i <= n - 2; i += 2)
+            {
+                res[i] = n - i;
+                res[i + 1] = -(n - i);
+            }
+            return res;
+        }
+    }
+}
