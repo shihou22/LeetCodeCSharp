b876f1b0b0b234bbfdda9fe9fae33dcafdbb9c5f
shihou22 <pc9801bx2@gmail.com>
2020-01-11 16:18:07 +0900

    practice

--- src/medium/Multiply Strings/Program.cs
+++ src/medium/Multiply Strings/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Text;
+using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace Multiply_Strings
 {
@@ -15,6 +18,8 @@ namespace Multiply_Strings
     }
     public string Multiply(string num1, string num2)
     {
+      if (num1.Max() == '0' || num2.Max() == '0')
+        return "0";
       int max = Math.Max(num1.Length, num2.Length);
       int min = Math.Min(num1.Length, num2.Length);
       string maxS = "", minS = "";
@@ -36,27 +41,41 @@ namespace Multiply_Strings
         string wkRes = "";
         for (int j = 0; j < max; j++)
         {
-          int wkMax = maxS[max - 1 - i] - '0';
+          int wkMax = maxS[max - 1 - j] - '0';
           int wk = wkMax * wkIn + carry;
-          carry = (wk % 10);
+          carry = wk / 10;
           wk %= 10;
           wkRes = wk + wkRes;
         }
         if (carry != 0)
           wkRes = carry + "" + wkRes;
 
-        carry = 0;
-        for (int j = 0; j < wkRes.Length; j++)
+        if (res.Length == 0)
         {
-          int wkP1 = wkRes[wkRes.Length - 1 - j] - '0';
-          int wkP2 = res[res.Length - 1 - j - i] - '0';
-          int wk = wkP1 + wkP2 + carry;
-          carry = (wk % 10);
+          res = wkRes;
+          continue;
+        }
+
+        int addMax = Math.Max(res.Length - i, wkRes.Length);
+        carry = 0;
+        String wkRes2 = res.Substring(res.Length - i);
+        for (int j = 0; j < addMax; j++)
+        {
+          int wkMax = 0;
+          if (res.Length - 1 - i - j >= 0)
+            wkMax = res[res.Length - 1 - i - j] - '0';
+          int wk = 0;
+          if (wkRes.Length - 1 - j >= 0)
+            wk = wkRes[wkRes.Length - 1 - j] - '0';
+
+          wk = wk + wkMax + carry;
+          carry = wk / 10;
           wk %= 10;
-          res[res.Length - 1 - j - i] = wk;
+          wkRes2 = wk + wkRes2;
         }
         if (carry != 0)
-
+          wkRes2 = carry + wkRes2;
+        res = wkRes2;
       }
       return res;
     }
