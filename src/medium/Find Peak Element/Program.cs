4191a291f9814a9a898822d29b2c15dac4cfa8ee
shihou22 <pc9801bx2@gmail.com>
2020-01-13 14:58:17 +0900

    practice

---
+++ src/medium/Find Peak Element/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Find_Peak_Element
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+    public int FindPeakElement(int[] nums)
+    {
+      int peak = nums[0];
+      int cnt = 0;
+      for (int i = 0; i < nums.Length; i++)
+      {
+        cnt = i;
+        if (peak > nums[i])
+          return i - 1;
+
+        if (peak < nums[i])
+        {
+          peak = nums[i];
+        }
+      }
+      return cnt;
+    }
+  }
+}
