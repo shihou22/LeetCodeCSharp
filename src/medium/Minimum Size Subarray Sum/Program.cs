e1a795ee2ddb1009f84a165327e78a5dcbcdc379
shihou22 <pc9801bx2@gmail.com>
2020-01-18 22:10:52 +0900

    practice

---
+++ src/medium/Minimum Size Subarray Sum/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Minimum_Size_Subarray_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine(program.MinSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 }));
+            Console.WriteLine(program.MinSubArrayLen(3, new int[] { 1, 1 }));
+            Console.WriteLine("Hello World!");
+        }
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            int[] rui = new int[nums.Length + 1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                rui[i + 1] = nums[i] + rui[i];
+            }
+            int res = nums.Length + 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int wk = (s - nums[i]) + rui[i + 1];
+                int index = lowerBound(rui, wk);
+                if (index >= 0 && index <= nums.Length && index - i >= 0)
+                {
+                    res = Math.Min(res, index - i);
+                }
+            }
+            return res != nums.Length + 1 ? res : 0;
+        }
+        /*
+        LowerBound実装
+         */
+        private int lowerBound(int[] nums, long val)
+        {
+            int left = -1;
+            int right = nums.Length;
+            while (right - left > 1)
+            {
+                int mid = left + (right - left) / 2;
+                if (val <= nums[mid])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+            return right;
+        }
+
+        public int MinSubArrayLenSlidingWindow(int s, int[] nums)
+        {
+            int sum = 0;
+            int right = 0;
+            int res = nums.Length + 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (right < nums.Length && sum < s)
+                {
+                    sum += nums[right];
+                    right++;
+                }
+                if (sum >= s)
+                {
+                    res = Math.Min(res, right - i);
+                    sum -= nums[i];
+                }
+            }
+
+            return res != nums.Length + 1 ? res : 0;
+        }
+    }
+}
--- src/sample/Program.cs
+++ src/sample/Program.cs
@@ -10,6 +10,59 @@ namespace sample
             Console.WriteLine(getNum(new int[] { 3, 9, 24, 46, 88, 122, 158, 200, 367, 555, 912, 1543, 2323 }));
             // Console.WriteLine("1: " + fact(4));
             // Console.WriteLine("2: " + fact2(4, 1));
+
+            char[] wk1 = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] wk2 = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            for (int i = 0; i < wk1.Length; i++)
+            {
+                if (i < wk1.Length)
+                    Console.WriteLine(wk1[i] + "-" + (char)(wk1[i] ^ 32));
+                if (i < wk2.Length)
+                    Console.WriteLine(wk2[i] + "-" + (char)(wk2[i] ^ 32));
+            }
+        }
+
+        private string kujibiki(int[] nums, int m)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                        for (int l = 0; l < n; l++)
+                            if (nums[i] + nums[j] + nums[k] + nums[l] == m)
+                                return "Yes";
+
+            return "No";
+        }
+        private string kujibiki2(int[] nums, int m)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                    {
+                        int wk = m - (nums[i] + nums[j] + nums[k]);
+                        if (Array.BinarySearch(nums, wk) >= 0)
+                            return "Yes";
+                    }
+            return "No";
+        }
+        private string kujibiki3(int[] nums, int m)
+        {
+            int n = nums.Length;
+            int[] memo = new int[n * n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    memo[i * n + j] = nums[i] + nums[j];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    int wk = m - (nums[i] + nums[j]);
+                    if (Array.BinarySearch(memo, wk) >= 0)
+                        return "Yes";
+                }
+            return "No";
         }
         static int getNum(int[] nums)
         {
