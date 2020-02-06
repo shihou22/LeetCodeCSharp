ad5e0081930ec12b222bc8e0e469b2edf5d7c22f
shihou22 <pc9801bx2@gmail.com>
2020-02-06 12:53:59 +0900

    pratice

---
+++ src/medium/H-Index/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace H_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine(program.HIndex(new int[] { 3, 0, 6, 1, 5 }));
+            Console.WriteLine("Hello World!");
+        }
+        public int HIndex(int[] citations)
+        {
+            if (citations.Length == 0)
+                return 0;
+            Array.Sort(citations, (x, y) => -x.CompareTo(y));
+            int res = 0;
+            int n = citations.Length;
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if ((i + 1) <= citations[i])
+                    res = i + 1;
+            }
+            return res;
+        }
+    }
+}
