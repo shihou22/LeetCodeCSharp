6b3687b3102baacd41daf1bf02759e1333d74a19
shihou22 <pc9801bx2@gmail.com>
2020-02-07 10:59:57 +0900

    practice

--- src/medium/Total Hamming Distance/Program.cs
+++ src/medium/Total Hamming Distance/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Total_Hamming_Distance
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Program program = new Program();
+      //6
+      Console.WriteLine(program.TotalHammingDistance(new int[] { 4, 14, 2 }));
+      Console.WriteLine("Hello World!");
     }
+    public int TotalHammingDistance(int[] nums)
+    {
+      int res = 0;
+      for (int i = 0; i < 32; i++)
+      {
+        int cntZero = 0;
+        int cntOne = 0;
+        for (int j = 0; j < nums.Length; j++)
+        {
+          if ((nums[j] & 1) == 0)
+            cntZero++;
+          else
+            cntOne++;
+
+          nums[j] >>= 1;
+        }
+        res += (cntZero * cntOne);
+      }
+      return res;
+    }
+  }
 }
