20fcaa8bc41ba73b7a79ea890b2796dab25e95cd
shihou22 <pc9801bx2@gmail.com>
2020-02-09 11:47:50 +0900

    practice

---
+++ src/easy/Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Number_of_Steps_to_Reduce_a_Number_to_Zero
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program program = new Program();
+      //   6
+      Console.WriteLine(program.NumberOfSteps(14));
+      // 4
+      Console.WriteLine(program.NumberOfSteps(8));
+      // 12
+      Console.WriteLine(program.NumberOfSteps(123));
+
+      Console.WriteLine("Hello World!");
+    }
+    public int NumberOfSteps(int num)
+    {
+      return num == 0 ? 0 : 1 + (((num % 2) == 0) ? NumberOfSteps(num >> 1) : NumberOfSteps(--num));
+    }
+    public int NumberOfStepsIteration(int num)
+    {
+      int cnt = 0;
+      while (num > 0)
+      {
+        if (num % 2 == 0)
+          num >>= 1;
+        else
+          num--;
+        cnt++;
+      }
+      return cnt;
+    }
+  }
+}
