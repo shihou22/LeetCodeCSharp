2e90bd76cb7265d24ad9801ab34c0f81cc5a984a
shihou22 <pc9801bx2@gmail.com>
2020-02-09 11:31:16 +0900

    practice

---
+++ src/easy/The K Weakest Rows in a Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System;
+
+namespace The_K_Weakest_Rows_in_a_Matrix
+{
+  class Pair<K, V>
+  {
+    public K key { get; }
+    public V val { get; }
+    public Pair(K k, V v)
+    {
+      this.key = k;
+      this.val = v;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+    public int[] KWeakestRows(int[][] mat, int k)
+    {
+      return mat.Select((val, index) => new Pair<int, int>(val.Where(x => (x == 1)).Count(), index)).OrderBy(x => x.key).Take(k).Select(x => x.val).ToArray();
+    }
+  }
+}
