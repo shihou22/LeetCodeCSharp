545d3ec6ff490d27ca3c45bee023da34a9ea17e8
shihou22 <pc9801bx2@gmail.com>
2020-02-14 09:13:09 +0900

    practice

---
+++ src/medium/Group the People Given the Group Size They Belong To/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Group_the_People_Given_the_Group_Size_They_Belong_To
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public IList<IList<int>> GroupThePeople(int[] groupSizes)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            Dictionary<int, IList<int>> tmp = new Dictionary<int, IList<int>>();
+            for (int i = 0; i < groupSizes.Length; i++)
+            {
+                int wk = groupSizes[i];
+                if (!tmp.ContainsKey(wk))
+                    tmp.Add(wk, new List<int>());
+                tmp[wk].Add(i);
+                if (tmp[wk].Count == wk)
+                {
+                    res.Add(tmp[wk]);
+                    tmp.Remove(wk);
+                }
+            }
+            return res;
+
+        }
+    }
+}
