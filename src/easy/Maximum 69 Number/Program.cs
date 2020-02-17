143b6e3ee4acbe90aedf79e7a2262fb1146ed1ef
shihou22 <pc9801bx2@gmail.com>
2020-02-17 14:08:00 +0900

    practice

---
+++ src/easy/Maximum 69 Number/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maximum_69_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //9969
+            Console.WriteLine(program.Maximum69Number(9669));
+            //9999
+            Console.WriteLine(program.Maximum69Number(9996));
+            Console.WriteLine("Hello World!");
+        }
+        public int Maximum69Number(int num)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (num != 0)
+            {
+                stack.Push(num % 10);
+                num /= 10;
+            }
+            bool isFirst = true;
+            while (stack.Count > 0)
+            {
+                int wk = stack.Pop();
+                if (wk == 6 && isFirst)
+                {
+                    isFirst = false;
+                    wk += 3;
+                }
+                num *= 10;
+                num += wk;
+            }
+            return num;
+        }
+        public int Maximum69NumberStr(int num)
+        {
+            string wk = num.ToString();
+            int index = -1;
+            for (int i = 0; i < wk.Length; i++)
+            {
+                if ((wk[i] - '0') == 6)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index != -1)
+            {
+                num += 3 * (int)Math.Pow(10, wk.Length - 1 - index);
+            }
+            return num;
+        }
+    }
+}
---
+++ src/medium/Push Dominoes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+
+namespace Push_Dominoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //"LL.RR.LLRRLL.."
+            Console.WriteLine(program.PushDominoes(".L.R...LR..L.."));
+            //"RR.L"
+            Console.WriteLine(program.PushDominoes("RR.L"));
+            // //"LL.RR"
+            Console.WriteLine(program.PushDominoes(".L.R."));
+            // //"LR"
+            Console.WriteLine(program.PushDominoes("LR"));
+            // //"RRR.L"
+            Console.WriteLine(program.PushDominoes("R.R.L"));
+            Console.WriteLine("Hello World!");
+        }
+        public string PushDominoes(string dominoes)
+        {
+            char[] toward = dominoes.ToCharArray();
+            int[] towardRight = new int[dominoes.Length];
+            int[] towardLeft = new int[dominoes.Length];
+            int n = dominoes.Length;
+            int index = 0;
+            int left = index;
+            int right = index;
+            for (int i = 0; i < n; i++)
+            {
+                towardRight[i] = toward[i] == 'R' ? 1 : 0;
+                towardLeft[n - 1 - i] = toward[n - 1 - i] == 'L' ? -1 : 0;
+                if (i != 0)
+                {
+                    if (toward[i] == '.' && towardRight[i - 1] > 0)
+                        towardRight[i] = towardRight[i - 1] + 1;
+                    if (toward[n - 1 - i] == '.' && towardLeft[n - i] < 0)
+                        towardLeft[n - 1 - i] = towardLeft[n - i] - 1;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (toward[i] != '.')
+                    continue;
+                if (towardRight[i] > 0 && towardLeft[i] < 0)
+                {
+                    int tmp = towardRight[i] + towardLeft[i];
+                    if (tmp > 0)
+                    {
+                        toward[i] = 'L';
+                    }
+                    else if (tmp < 0)
+                    {
+                        toward[i] = 'R';
+                    }
+                }
+                else if (towardRight[i] > 0)
+                {
+                    toward[i] = 'R';
+                }
+                else if (towardLeft[i] < 0)
+                {
+                    toward[i] = 'L';
+                }
+
+            }
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in toward)
+            {
+                builder.Append(item);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
