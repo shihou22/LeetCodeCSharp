23644420c0064a04ba3299fd1a94d603cbe0216a
shihou22 <pc9801bx2@gmail.com>
2020-02-18 20:35:42 +0900

    practice

---
+++ src/easy/Count Negative Numbers in a Sorted Matrix/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Count_Negative_Numbers_in_a_Sorted_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int CountNegatives(int[][] grid)
+        {
+            int cnt = grid.Select(x => x.Where(y => y < 0).Count()).Sum();
+            return cnt;
+        }
+    }
+}
