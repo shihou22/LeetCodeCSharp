04bb87d3fb5e40e31ce00dae1a808a550a7c7c11
shihou22 <pc9801bx2@gmail.com>
2020-02-18 11:49:32 +0900

    practice

---
+++ src/medium/Insert into a Binary Search Tree/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insert_into_a_Binary_Search_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class TreeNodeHelper
+    {
+        public static TreeNode CreateTree(int[] nums)
+        {
+            return CreateTree(nums.Select(x => x.ToString()).ToArray());
+        }
+
+        public static TreeNode CreateTree(string[] nums)
+        {
+            if (nums == null)
+                return null;
+            TreeNode res = new TreeNode(int.Parse(nums[0]));
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            nodes.Enqueue(res);
+            int index = 1;
+            while (nodes.Count > 0)
+            {
+                int cnt = nodes.Count;
+                for (int i = 0; i < cnt; i++)
+                {
+                    TreeNode root = nodes.Dequeue();
+                    if (root == null)
+                        continue;
+                    root.left = GetNode(nums, index++);
+                    root.right = GetNode(nums, index++);
+                    nodes.Enqueue(root.left);
+                    nodes.Enqueue(root.right);
+                }
+            }
+            return res;
+        }
+        private static TreeNode GetNode(string[] nums, int n)
+        {
+            if (n >= nums.Length)
+                return null;
+            if (nums[n] == null)
+                return null;
+            return new TreeNode(int.Parse(nums[n]));
+        }
+        public static string ResultStr(TreeNode node)
+        {
+            StringBuilder builder = new StringBuilder();
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            while (nodes.Count > 0)
+            {
+                int cnt = nodes.Count;
+                for (int i = 0; i < cnt; i++)
+                {
+                    TreeNode wk = nodes.Dequeue();
+                    if (wk == null)
+                    {
+                        builder.Append("null").Append(" ");
+                    }
+                    else
+                    {
+                        builder.Append(wk.val).Append(" ");
+                        nodes.Enqueue(wk.left);
+                        nodes.Enqueue(wk.right);
+                    }
+                }
+            }
+            return builder.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return root;
+            if (root.val > val)
+            {
+                if (root.left != null)
+                    InsertIntoBST(root.left, val);
+                else
+                    root.left = new TreeNode(val);
+            }
+            else
+            {
+                if (root.right != null)
+                    InsertIntoBST(root.right, val);
+                else
+                    root.right = new TreeNode(val);
+            }
+            return root;
+        }
+
+    }
+}
