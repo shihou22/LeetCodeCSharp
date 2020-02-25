f975338ba3a4d22c02e44180c5c9e820d83915be
shihou22 <pc9801bx2@gmail.com>
2020-02-25 11:06:01 +0900

    practice

---
+++ src/medium/Champagne Tower/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Champagne_Tower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public double ChampagneTower(int poured, int query_row, int query_glass)
+        {
+            double[,] glasses = new double[102, 102];
+            glasses[0, 0] = poured;
+            for (int i = 0; i < query_row; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    double p = (glasses[i, j] - 1L) / 2L;
+                    if (p > 0L)
+                    {
+                        glasses[i + 1, j] += p;
+                        glasses[i + 1, j + 1] += p;
+                    }
+                }
+            }
+            return Math.Min(1, glasses[query_row, query_glass]);
+
+
+        }
+
+    }
+}
