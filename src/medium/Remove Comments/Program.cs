2d1df56a17eba00dd48ee94852e978d682ef2c0d
shihou22 <pc9801bx2@gmail.com>
2020-03-10 17:46:11 +0900

    practice

---
+++ src/medium/Remove Comments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Remove_Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public IList<string> RemoveComments(string[] source)
+        {
+
+        }
+    }
+}
